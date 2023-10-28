f5b71e55905bf49cb11fe24009f0d0345e0dde4b
Dileep Bukkuru <dileepbukkuru20@gmail.com>
2023-10-28 17:55:46 +0530

    Admin  login Corner cases fixed,duplicate code removed.

--- Session.php
+++ Session.php
@@ -9,31 +9,35 @@ $dbname = 'dynamic';
 $conn = mysqli_connect($host, $user, $pass, $dbname);
 if (!$conn) {
     die('Could not connect: ' . mysqli_connect_error());
-}   
+}
 
- if (isset($_POST["submit"])) {
+if (isset($_POST["submit"])) {
     $userid = $_POST["Auserid"];
     $password = $_POST["Apassword"];
-     $result = mysqli_query($conn, "SELECT * FROM admin_users WHERE Admin_userid = '$userid'");
+    $result = mysqli_query($conn, "SELECT * FROM admin_users WHERE Admin_userid = '$userid'");
     $row = mysqli_fetch_assoc($result);
 
     if (mysqli_num_rows($result) > 0) {
         if ($password == $row["password"]) {
-             $result_all_form = mysqli_query($conn, "SELECT * FROM admin_users WHERE Admin_userid = '$userid'");
-            
+            $result_all_form = mysqli_query($conn, "SELECT * FROM admin_users WHERE Admin_userid = '$userid'");
+
             $all_form = mysqli_fetch_all($result_all_form, MYSQLI_ASSOC);
             $_SESSION["submit"] = true;
             $_SESSION["userid"] = $row["Admin_userid"];
             $_SESSION["form"] = $all_form;
-            header("Location: Session2.php");
-            exit(); 
+            header("Location: Home1.php");
+            exit();
         } else {
-            echo "Invalid password";
-        }  
-    }
+            echo "<script>
+              alert('Invalid Password');
+             window.location.href='Admin.php';
+               </script>";
+        }
+    } else {
 
-    else {
-        echo "Invalid userid";
+        echo "<script>alert('Invalid UserId');
+                window.location.href='Admin.php';
+              </script>";
     }
 }
 
